7b845753384382080d2b589a381078d7ccc79ad1
TumanovaNelly <2-man.off@mail.ru>
2025-07-11 00:21:09 +0300

    feat(core): Add CellState and GameState enums

---
+++ core/src/main/java/com/example/minesweeper/core/model/CellState.java
@@ -0,0 +1,7 @@
+package com.example.minesweeper.core.model;
+
+public enum CellState {
+    HIDDEN,
+    REVEALED,
+    FLAGGED;
+}
---
+++ core/src/main/java/com/example/minesweeper/core/model/GameState.java
@@ -0,0 +1,8 @@
+package com.example.minesweeper.core.model;
+
+public enum GameState {
+    NOT_STARTED,
+    IN_PROGRESS,
+    WON,
+    LOST;
+}
